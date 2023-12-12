8e58fb3a4492082c113c13011d2615763e213cf2
abcd <abcd@woojinens.com>
2023-12-12 17:32:56 +0900

    ADD main.py

--- modbus_simulator/main.py
+++ modbus_simulator/main.py
@@ -2,6 +2,14 @@
 Modbus Simu App
 ===============
 '''
+
+import os
+os.environ["KIVY_NO_ARGS"] = "1"
+os.environ['KIVY_IMAGE'] = "pil,sdl2" # use pil instead of SDL2 image if you get the libpng16 error
+# you must add to the path the location of your SDL2 binaries
+os.environ['PATH'] += ';' + os.path.expandvars('C:\\dev_env\\Python\\Python3608\\share\\glew\\bin')
+os.environ['PATH'] += ';' + os.path.expandvars('C:\\dev_env\\Python\\Python3608\\share\\sdl2\\bin')
+
 import click
 import sys
 import six
